3e38b4169b051cef81ef77a57e9e8dd0c36423e0
djamat.zac <zac.djamat-dubois.auditeur@lecnam.net>
2026-03-07 19:54:04 +0100

    hotfix(alert): ajout du fichier manquant alert

---
+++ lyric-quiz/src/components/ui/alert.tsx
@@ -0,0 +1,69 @@
+import * as React from "react"
+import { cva, type VariantProps } from "class-variance-authority"
+
+import { cn } from "@/lib/utils"
+
+const alertVariants = cva(
+    "relative w-full rounded-xl border p-4 [&>svg~*]:pl-7 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground font-titre text-xl tracking-wide shadow-lg",
+    {
+        variants: {
+            variant: {
+                default: "bg-background text-foreground",
+                destructive:
+                    "border-destructive/50 text-destructive dark:border-destructive [&>svg]:text-destructive bg-destructive/10 shadow-[0_0_15px_rgba(255,77,79,0.2)]",
+                // NOUVEAU : On ajoute ta variante de victoire !
+                success:
+                    "border-secondary text-secondary bg-secondary/10 shadow-[0_0_15px_rgba(64,201,255,0.2)] animate-pulse",
+            },
+        },
+        defaultVariants: {
+            variant: "default",
+        },
+    }
+)
+
+function Alert({
+  className,
+  variant,
+  ...props
+}: React.ComponentProps<"div"> & VariantProps<typeof alertVariants>) {
+  return (
+    <div
+      data-slot="alert"
+      role="alert"
+      className={cn(alertVariants({ variant }), className)}
+      {...props}
+    />
+  )
+}
+
+function AlertTitle({ className, ...props }: React.ComponentProps<"div">) {
+  return (
+    <div
+      data-slot="alert-title"
+      className={cn(
+        "col-start-2 line-clamp-1 min-h-4 font-medium tracking-tight",
+        className
+      )}
+      {...props}
+    />
+  )
+}
+
+function AlertDescription({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  return (
+    <div
+      data-slot="alert-description"
+      className={cn(
+        "col-start-2 grid justify-items-start gap-1 text-sm text-muted-foreground [&_p]:leading-relaxed",
+        className
+      )}
+      {...props}
+    />
+  )
+}
+
+export { Alert, AlertTitle, AlertDescription }
